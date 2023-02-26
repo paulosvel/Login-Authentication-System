c60fc667490e82a37fc5a7bd9092daa18d981225
paulosvel <paul2001vel@gmail.com>
2023-02-26 19:36:55 +0200

    added datetime in msg

--- application/models/MessageModel.php
+++ application/models/MessageModel.php
@@ -2,11 +2,15 @@
 class MessageModel extends CI_Model{
 
     public function create(){
+        $this->load->helper('date');
+        $date = new DateTime('now', new DateTimeZone('GMT'));
+        $date->setTimezone(new DateTimeZone('Europe/Athens'));
+        $created_at = $date->format('Y-m-d H:i:s');
 
 
         $data = array(
             'body_text' => $this->input->post('body_text'),
-            'created_at' => $this->input->post('created_at'),
+            'created_at' => $created_at,
             'user_id' => $this->session->userdata('user_id'),
         );
 
